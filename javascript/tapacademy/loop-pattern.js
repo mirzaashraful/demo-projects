a8397d8dc70b97eea12b502d3d22f6e308ad75e2
mirzaashraful <mirza.ashraful@gmail.com>
2025-10-20 12:11:27 +0600

    loop pattern

---
+++ javascript/tapacademy/loop-pattern.js
@@ -0,0 +1,45 @@
+//https://www.youtube.com/watch?v=nFR7ef84rfY&list=PLU83Ru7iGtAtTlQ8MRGHCBu4tzATiRfmY&index=11
+// Example 1: Hollow Square Pattern
+// let i;
+// let j;
+// let row;
+// let n = 5;
+
+// for (i=1; i<=n; i++){
+//     row = '';
+//     for (j=1; j<=n; j++){
+//        if(i == 1 || i == n || j == 1 || j == n){
+//         row += '* ';
+//     } else {
+//         row += '  ';
+//     }
+// }
+//     console.log(row);
+// }
+
+
+
+// Example 2: Right-Angled Triangle Pattern
+
+let i;
+let j;
+let row;
+let n = 11;
+
+console.log(Math.floor(n/2));
+
+for (i=0; i<n; i++){
+    row = '';
+    for (j=0; j<n; j++){
+       if(i == 0 || i == n-1 || j == 0 || j == n-1 || j == Math.floor(n/2) 
+        || i == Math.floor(n/2) || i == j || i + j == n - 1
+        || i + j == Math.floor(n/2) || i - j == Math.floor(n/2)
+        || i+j == (n - 1) + Math.floor(n/2)
+        || j - i == Math.floor(n/2)){
+        row += '* ';
+    } else {
+        row += '  ';
+    }
+}
+    console.log(row);
+}
